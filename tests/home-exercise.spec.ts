5df56cba37d1cb5ec115cc286d36f6151e8db8d4
Rogério Castro <rcastro@letsgetchecked.com>
2024-12-09 17:18:31 +0000

    update tests

--- tests/home-exercise.spec.ts
+++ tests/home-exercise.spec.ts
@@ -1,35 +1,19 @@
 import { expect, test, type Page }from "@playwright/test";
 import { faker } from '@faker-js/faker';
 
-/*
-NOTAS INTERESSANTES:
-- Uso de objectos para guardar dados de testes
-- Uso de funções para encapsular ações
-- Faker para gerar dados de teste
-
-MELHORIAS:
-- Uso de before each para registo evita erro na validaçao da row (falha na segunda execuçao)
-*/
-
 const testUser = {
-    firstName: "Snoopy",
-    lastName: "Dog",
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
     email: faker.internet.email(),
-    password: "Snoopy123"
-};
-
-const testContact = {
-    firstName: "Snoopy",
-    lastName: "Dog",
-    birthdate: "1997-12-25",
-    email: "kemokox372@ikowat.com",
-    phone: "258741963",
-    street1: "Rua Nova 4",
-    street2: "",
-    city: "Porto",
-    stateProvince: "Porto",
-    postalCode: "12345",
-    country: "Portugal"
+    password: faker.internet.password(),
+    birthdate: faker.date.past(),
+    phone: faker.phone.phoneNumber(),
+    street1: faker.address.streetName(),
+    street2: faker.address.secondaryAddress(),
+    city: faker.address.city(),
+    stateProvince: faker.address.state(),
+    postalCode: faker.address.zipCode(),
+    country: faker.address.country()
 };
 
 test.describe("Contact List App Tests", () => {
@@ -67,16 +51,16 @@ test.describe("Contact List App Tests", () => {
         await expect(page.locator("h1")).toHaveText("Add Contact");
 
         // fill contact form
-        await page.getByRole("textbox", {name: "First Name"}).fill(testContact.firstName);
-        await page.getByRole("textbox", {name: "Last Name"}).fill(testContact.lastName);
-        await page.getByRole("textbox", {name: "Date of Birth:"}).fill(testContact.birthdate);
-        await page.getByRole("textbox", {name: "Email"}).fill(testContact.email);
-        await page.getByRole("textbox", {name: "Phone"}).fill(testContact.phone);
-        await page.getByRole("textbox", {name: "Address 1"}).fill(testContact.street1);
-        await page.getByRole("textbox", {name: "City"}).fill(testContact.stateProvince);
-        await page.getByRole("textbox", {name: "State or Province"}).fill(testContact.city);
-        await page.getByRole("textbox", {name: "Postal Code"}).fill(testContact.postalCode);
-        await page.getByRole("textbox", {name: "Country"}).fill(testContact.country);
+        await page.getByRole("textbox", {name: "First Name"}).fill(testUser.firstName);
+        await page.getByRole("textbox", {name: "Last Name"}).fill(testUser.lastName);
+        await page.getByRole("textbox", {name: "Date of Birth:"}).fill(testUser.birthdate);
+        await page.getByRole("textbox", {name: "Email"}).fill(testUser.email);
+        await page.getByRole("textbox", {name: "Phone"}).fill(testUser.phone);
+        await page.getByRole("textbox", {name: "Address 1"}).fill(testUser.street1);
+        await page.getByRole("textbox", {name: "City"}).fill(testUser.stateProvince);
+        await page.getByRole("textbox", {name: "State or Province"}).fill(testUser.city);
+        await page.getByRole("textbox", {name: "Postal Code"}).fill(testUser.postalCode);
+        await page.getByRole("textbox", {name: "Country"}).fill(testUser.country);
 
         //Submit row
 
@@ -84,22 +68,22 @@ test.describe("Contact List App Tests", () => {
         await expect(page.locator("h1")).toHaveText("Contact List");
 
         // Click row
-        const rowName = page.getByRole('cell', { name: testContact.firstName });
+        const rowName = page.getByRole('cell', { name: testUser.firstName });
         await rowName.click();
 
         // Validate row
         await expect(page.locator("h1")).toHaveText("Contact Details");
 
-        await expect(page.locator('#firstName')).toHaveText(testContact.firstName);
-        await expect(page.locator('#lastName')).toHaveText(testContact.lastName);
-        await expect(page.locator('#birthdate')).toHaveText(testContact.birthdate);
-        await expect(page.locator('#email')).toHaveText(testContact.email);
-        await expect(page.locator('#phone')).toHaveText(testContact.phone);
-        await expect(page.locator('#street1')).toHaveText(testContact.street1);
-        await expect(page.locator('#street2')).toHaveText(testContact.street2);
-        await expect(page.locator('#city')).toHaveText(testContact.city);
-        await expect(page.locator('#stateProvince')).toHaveText(testContact.stateProvince);
-        await expect(page.locator('#postalCode')).toHaveText(testContact.postalCode);
-        await expect(page.locator('#country')).toHaveText(testContact.country);
+        await expect(page.locator('#firstName')).toHaveText(testUser.firstName);
+        await expect(page.locator('#lastName')).toHaveText(testUser.lastName);
+        await expect(page.locator('#birthdate')).toHaveText(testUser.birthdate);
+        await expect(page.locator('#email')).toHaveText(testUser.email);
+        await expect(page.locator('#phone')).toHaveText(testUser.phone);
+        await expect(page.locator('#street1')).toHaveText(testUser.street1);
+        await expect(page.locator('#street2')).toHaveText(testUser.street2);
+        await expect(page.locator('#city')).toHaveText(testUser.city);
+        await expect(page.locator('#stateProvince')).toHaveText(testUser.stateProvince);
+        await expect(page.locator('#postalCode')).toHaveText(testUser.postalCode);
+        await expect(page.locator('#country')).toHaveText(testUser.country);
     });
 });
