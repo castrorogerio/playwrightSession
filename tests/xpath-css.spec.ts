23fd5686913bd976f51755afb2c80ba0b7201648
Rogério Castro <rcastro@letsgetchecked.com>
2024-12-11 20:02:12 +0000

    context demo

--- tests/xpath-css.spec.ts
+++ tests/xpath-css.spec.ts
@@ -1,20 +1,26 @@
-import { expect, test } from '@playwright/test';
+import { BrowserContext, expect, test, type Page } from '@playwright/test';
 
 test.describe ( 'XPath and CSS', () => {
+    let context: BrowserContext;
+    let batatas: Page, cebolas: Page;
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto("https://dsportalapp.herokuapp.com/home");
+    test.beforeEach(async ({browser}) => {
+        context = await browser.newContext();
     });
 
-    test("use xpath", async ({ page }) => {
-        await expect(page.locator('//h5[@class="card-title"][contains(text(), "Array")]/following-sibling::a[1]')).toHaveText('Get Started');
+    test("use xpath", async () => {
+        batatas = await context.newPage();
+        await batatas.goto("https://dsportalapp.herokuapp.com/home");
+        await expect(batatas.locator('//h5[@class="card-title"][contains(text(), "Array")]/following-sibling::a[1]')).toHaveText('Get Started');
     });
 
-    test("use css", async ({ page }) => {
-        await expect(page.locator('h5.card-title:has-text("Array") + p + a')).toHaveText('Get Started');
+    test("use css", async () => {
+        cebolas = await context.newPage();
+        await cebolas.goto("https://dsportalapp.herokuapp.com/home");
+        await expect(cebolas.locator('h5.card-title:has-text("Array") + p + a')).toHaveText('Get Started');
     });
 
-    test.afterEach(async ({ page }) => {
-        await page.close();
+    test.afterEach(async () => {
+        await context.close();
     });
 });
