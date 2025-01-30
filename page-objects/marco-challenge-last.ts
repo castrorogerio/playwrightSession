5525dc8d23d979eed31a3aa8d1841a60d88630f6
Marco Martins <sequirei.costa11@icloud.com>
2025-01-30 19:56:14 +0000

    Added before each assertion

--- page-objects/marco-challenge-last.ts
+++ page-objects/marco-challenge-last.ts
@@ -2,8 +2,12 @@ import { Locator, Page } from "@playwright/test";
 
 export class blipWebsite {
     readonly url: string;
-    
+
     constructor(private page: Page) {
         this.url = "www.blip.pt";
+    }
+
+    async goToPage() {
+        await this.page.goto(this.url);
     }
 }
--- tests/uiProject/marcoChallenge.spec.ts
+++ tests/uiProject/marcoChallenge.spec.ts
@@ -1,5 +1,16 @@
-import { test } from "@playwright/test";
+import { BrowserContext, Page, test } from "@playwright/test";
+import { blipWebsite } from "../../page-objects/marco-challenge-last";
 
 test.describe('Last Challenge', () => {
-    
+    let context: BrowserContext;
+    let page: Page;
+    let blipPage: blipWebsite;
+
+    test.beforeEach(async ({ browser }) => {
+        context = await browser.newContext();
+        page = await context.newPage();
+        blipPage = new blipWebsite(page);
+
+        await blipPage.goToPage();
+    })
 });
