ec000a93b8fbc11abc42b8b97d14ab4c2e0d16dc
Carla <100221800+COliveira16@users.noreply.github.com>
2025-02-02 22:11:37 +0000

    ChallengeFinal

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,15 @@ export default defineConfig({
   workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    viewport: {
+      width: 1024,
+      height: 768
+    }
   },
 
   /* Configure projects for major browsers */
@@ -38,25 +40,9 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
+      use: { ...devices['Desktop Chrome'] },
     }
   ]
 });
