096fa75727cc29e99f996edc8e86daa613d182b0
Marco Martins <sequirei.costa11@icloud.com>
2025-02-04 15:01:31 +0000

    finish UI exercise

--- tests/uiProject/marcoChallenge.spec.ts
+++ tests/uiProject/marcoChallenge.spec.ts
@@ -24,7 +24,7 @@ test.describe('Last Challenge', () => {
         await expect(resultsUnitedKingdom).toContainText('Sorry, no jobs were found matching your criteria.'); // Check no results on search
     });
 
-    test('1 - Challenge Search QA Jobs in Portugal', async () => {
+    test.only('1 - Challenge Search QA Jobs in Portugal', async () => {
         await blipPage.goToJobs();
         // First option: Text to search, Second option: Country between Portugal or United Kingdom
         await blipPage.search('QA', 'Portugal');
@@ -34,6 +34,31 @@ test.describe('Last Challenge', () => {
 
         const jobCard1 = page.locator('.card-body').first();
         await expect(jobCard1).toContainText('Portugal');
+
+        // Save some result on fav jobs
+        const save = page.locator('.btn-add-job ').first();
+        await save.click();
+        await page.waitForTimeout(1500);
+
+        // Check the save jobs to see if there is any item
+        const savedJobsPage = page.locator('#js-saved-jobs-page');
+        await savedJobsPage.click();
+
+        const titlePage = page.locator('.hero-heading');
+        await expect(titlePage).toBeVisible();
+
+        // Check if there is any card, if so it means some job was saved
+        const jobSaved = page.locator('.card-body');
+        await expect(jobSaved).toBeVisible();
+
+        // remove all and check to assert the action was successful
+        await page.locator('#btn-remove-all-jobs').click();
+
+        await expect(jobSaved).not.toBeVisible();
+
+        const noJobs = page.locator('#js-no-saved-jobs');
+        await expect(noJobs).toBeVisible();
+        await expect(noJobs).toContainText("You don't currently have any saved jobs.")
     })
 
     
